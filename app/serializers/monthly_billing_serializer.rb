317ccf4391670e1c2e7c78b8feebcd2446e61260
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2016-02-09 19:34:17 +0900

    set attributes

--- app/serializers/monthly_billing_serializer.rb
+++ app/serializers/monthly_billing_serializer.rb
@@ -1,2 +1,3 @@
 class MonthlyBillingSerializer < ActiveModel::Serializer
+  attributes :id, :user_id, :item_code, :item_name, :item_price, :billing_type, :status, :registration_at, :canceled_at
 end
